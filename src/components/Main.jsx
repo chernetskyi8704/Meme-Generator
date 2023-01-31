50ee7672242cbe7c78c12ec55d662be7995ee196
Dmytro Chernetskyi <chernetskyi.private@gmail.com>
2023-01-31 14:33:49 +0200

    Added an asynchronous function that makes a request to the memes API and writes the received data to the memes variable.

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,18 @@
 import "../styles/Main.css";
 
 export function Main() {
+  let memes = [];
+
+  (async () => {
+    try {
+      const response = await fetch("https://api.imgflip.com/get_memes");
+      const data = await response.json();
+      memes = data.data.memes;
+    } catch (error) {
+      alert("Something went wrong, please refresh the page");
+    }
+  })();
+
   return (
     <main className="main">
       <section className="section__form">
